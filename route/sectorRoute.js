1a59543a869e6ac30aefc63afaa3b980b4b20abd
Baze19 <bazemontana@gmail.com>
2023-06-28 19:32:28 +0100

    getDetails

--- route/sectorRoute.js
+++ route/sectorRoute.js
@@ -1,10 +1,18 @@
-import express from 'express';
-import { createNewUser, getAllSectors, newSectors, updateUser } from "../controller/sector.js"
+import express from "express";
+import {
+  createNewUser,
+  getAllSectors,
+  newSectors,
+  updateUser,
+  getUserDetail,
+} from "../controller/sector.js";
 const router = express.Router();
 
-router.post('/newUser', createNewUser);
-router.get('/getAllSectors', getAllSectors);
-router.post('/newSector', newSectors);
-router.put( "/editUser/:id", updateUser);
+router.post("/newUser", createNewUser);
+router.get("/getAllSectors", getAllSectors);
+router.put("/editUser/:id", updateUser);
+router.post("/newSector", newSectors);
+
+router.get("/getUserDetail/:id", getUserDetail);
 
 export default router;
